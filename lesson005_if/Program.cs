ca02009ad83d6bf93bb6a649d923afa1a72ef42d
Jalini <jalini@mail.ru>
2022-10-22 22:10:54 +0300

    if

---
+++ lesson005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your name: ");
+string username=Console.ReadLine();
+if (username.ToLower()=="masha")
+{
+    Console.WriteLine("Ohh!MASHA!");
+}
+else
+{
+    Console.Write("hi!");
+    Console.WriteLine(username);
+}
